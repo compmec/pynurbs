a797f3b351aeb7a3e93460b0ded9391d7b300b2f
Carlos Adir <carlos.adir.leite@gmail.com>
2022-07-30 10:34:09 -0300

    Change test filename to knotoperations.

--- tests/test_knotoperations.py
+++ tests/test_knotoperations.py
@@ -19,7 +19,7 @@ def test_begin():
 @pytest.mark.order(4)
 @pytest.mark.timeout(2)
 @pytest.mark.dependency(depends=["test_begin"])
-def test_basefunction_basic():
+def test_insertknot_basefunction_basic():
     N = SplineBaseFunction([0, 0, 1, 1])
     newN = insert_knot_basefunction(N, 0.5)
     assert newN.U == [0, 0, 0.5, 1, 1]
@@ -38,26 +38,11 @@ def test_basefunction_basic():
     newN = insert_knot_basefunction(N, 0.5, 2)
     assert newN.U == [0, 0, 0, 0, 0.5, 0.5, 1, 1, 1, 1]
 
-@pytest.mark.order(4)
-@pytest.mark.timeout(2)
-@pytest.mark.dependency(depends=["test_basefunction_basic"])
-def test_basefunction_plot():
-    N = SplineBaseFunction([0, 0, 0, 0.1, 0.3, 0.4, 0.6, 0.7, 0.9, 1, 1, 1])
-    u = np.linspace(0, 1, 129)
-    Nu = N(u)
-    plt.figure()
-    for i in range(N.n):
-        plt.plot(u, Nu[i], color="r", ls="dotted")
-    N = insert_knot_basefunction(N, 0.5)
-    Nu = N(u)
-    for i in range(N.n):
-        plt.plot(u, Nu[i], color="b")
-    plt.legend()
 
 @pytest.mark.order(4)
 @pytest.mark.timeout(2)
-@pytest.mark.dependency(depends=["test_basefunction_basic"])
-def test_curve_basic():
+@pytest.mark.dependency(depends=["test_insertknot_basefunction_basic"])
+def test_insertknot_curve_basic():
     n, p = 10, 3
     U = getU_uniform(n=n, p=p)
     N = SplineBaseFunction(U)
@@ -72,38 +57,11 @@ def test_curve_basic():
     Cutest = newC(utest)
     np.testing.assert_almost_equal(Cugood, Cutest)
 
-@pytest.mark.order(4)
-@pytest.mark.timeout(2)
-@pytest.mark.dependency(depends=["test_curve_basic"])
-def test_curve_plot():
-    N = SplineBaseFunction([0, 0, 0, 0, 0.1, 0.25, 0.75, 0.9, 1, 1, 1, 1])
-    P = [[0, 1],
-         [1, 1],
-         [1, 0],
-         [2, 1],
-         [2, 2],
-         [3, 2],
-         [3, 3],
-         [4, 3]]
-    P = np.array(P)
-    C = SplineCurve(N, P)
-    newN = insert_knot_basefunction(N, 0.5)
-    newP = insert_knot_controlpoints(N.U, P, 0.5)
-    newC = SplineCurve(newN, newP)
-    u = np.linspace(0, 1, 129)
-    plt.figure()
-    plt.scatter(newP[:, 0], newP[:, 1], color="b")
-    plt.scatter(P[:, 0], P[:, 1], color="r")
-    Cu = C(u)
-    plt.plot(Cu[:, 0], Cu[:, 1], color="r", ls="dotted", label="old")
-    Cu = newC(u)
-    plt.plot(Cu[:, 0], Cu[:, 1], color="b", label="new")
-    plt.legend()
 
 @pytest.mark.order(4)
 @pytest.mark.timeout(5)
-@pytest.mark.dependency(depends=["test_curve_basic"])
-def test_with_geomdl():
+@pytest.mark.dependency(depends=["test_insertknot_curve_basic"])
+def test_insertknot_with_geomdl():
     """
     This function uses the library for Nurbs in, if the data are compatible
         https://github.com/orbingol/NURBS-Python
@@ -144,9 +102,9 @@ def test_end():
     pass
 
 def main():
-    test_basefunction_basic()
-    # test_basefunction_plot()
-    test_curve_basic()
+    test_insertknot_basefunction_basic()
+    test_insertknot_curve_basic()
+    test_insertknot_with_geomdl()
     # test_curve_plot()
 
 
