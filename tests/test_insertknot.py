5535427db19255dc50d496e46a64cf9d9e71dc6d
Carlos Adir <carlos.adir.leite@gmail.com>
2022-07-30 10:17:15 -0300

    Add test in insertknot to test with geomdl library

--- tests/test_insertknot.py
+++ tests/test_insertknot.py
@@ -1,9 +1,11 @@
 import pytest
 from compmec.nurbs import SplineBaseFunction
 from compmec.nurbs import SplineCurve
-from compmec.nurbs.knotspace import KnotVector, getU_uniform
+from compmec.nurbs.knotspace import KnotVector, getU_random, getU_uniform
 from compmec.nurbs.advanced import insert_knot_basefunction, insert_knot_controlpoints
 from matplotlib import pyplot as plt 
+from geomdl import BSpline
+from geomdl.operations import insert_knot
 import numpy as np
 
 @pytest.mark.order(4)
@@ -97,6 +99,43 @@ def test_curve_plot():
     Cu = newC(u)
     plt.plot(Cu[:, 0], Cu[:, 1], color="b", label="new")
     plt.legend()
+
+@pytest.mark.order(4)
+@pytest.mark.timeout(5)
+@pytest.mark.dependency(depends=["test_curve_basic"])
+def test_with_geomdl():
+    """
+    This function uses the library for Nurbs in, if the data are compatible
+        https://github.com/orbingol/NURBS-Python
+    """
+    ntests = 10
+    dim = 2
+    utest = np.linspace(0, 1, 129)
+    for i in range(ntests):
+        p = np.random.randint(1, 6)
+        n = 10*np.random.randint(p+1, p+11)
+        U = getU_random(n=n, p=p)
+        knotvector = KnotVector(U)
+        N = SplineBaseFunction(knotvector)
+        P = np.random.rand(n, dim)
+        Pgeomdl = []
+        for i, Pi in enumerate(P):
+            Pgeomdl.append(list(Pi))
+        Cgeomdl = BSpline.Curve()
+        Cgeomdl.degree = p
+        Cgeomdl.ctrlpts = Pgeomdl
+        Cgeomdl.knotvector = U
+
+        knot = 0.01+0.98*np.random.rand()
+        newN = insert_knot_basefunction(N, knot)
+        newP = insert_knot_controlpoints(N.U, P, knot)
+        newCcustom = SplineCurve(newN, newP)
+
+        insert_knot(obj=Cgeomdl, param=[knot], num=[1])
+        for i, ui in enumerate(utest):
+            Pcustom = newCcustom(ui)
+            Pgeomdl = Cgeomdl.evaluate_single(ui)
+            np.testing.assert_allclose(Pcustom, Pgeomdl)
 
 
 @pytest.mark.order(4)
