007b8e1a16b71786d2e0b6b901b94935c286e212
Carlos Adir <carlos.adir.leite@gmail.com>
2022-11-17 11:03:21 -0300

    Take out requirements file and put it into tox

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -1,41 +1,41 @@
+import numpy as np
 import pytest
-from compmec.nurbs import SplineBaseFunction
-from compmec.nurbs import SplineCurve
+
+from compmec.nurbs import SplineBaseFunction, SplineCurve
 from compmec.nurbs.curves import BaseCurve
 from compmec.nurbs.knotspace import GeneratorKnotVector
-import numpy as np
 
 
 @pytest.mark.order(3)
 @pytest.mark.dependency(
-	depends=["tests/test_basefunctions.py::test_end"],
-    scope='session')
+    depends=["tests/test_basefunctions.py::test_end"], scope="session"
+)
 def test_begin():
     pass
 
 
 @pytest.mark.order(3)
-@pytest.mark.timeout(2)
+@pytest.mark.timeout(5)
 @pytest.mark.dependency(depends=["test_begin"])
 def test_CreationSplineCurve():
     p = np.random.randint(1, 5)
-    n = np.random.randint(p+1, p+7)
-    n = p+1
+    n = np.random.randint(p + 1, p + 7)
+    n = p + 1
     U = GeneratorKnotVector.uniform(p=p, n=n)
-    N = SplineBaseFunction([0, 0, 1, 1]) # p = 1, n = 2
+    N = SplineBaseFunction([0, 0, 1, 1])  # p = 1, n = 2
     P = np.random.rand(2, 3)
     C = SplineCurve(N, P)
     assert isinstance(C, BaseCurve)
     assert callable(C)
- 
+
 
 @pytest.mark.order(3)
-@pytest.mark.timeout(2)
+@pytest.mark.timeout(5)
 @pytest.mark.dependency(depends=["test_CreationSplineCurve"])
 def test_SplineScalarCurve():
     npts = 129
     for p in range(1, 4):
-        for n in range(p+1, p+7):
+        for n in range(p + 1, p + 7):
             U = GeneratorKnotVector.random(p=p, n=n)
             N = SplineBaseFunction(U)
             P = np.random.rand(n)
@@ -44,14 +44,15 @@ def test_SplineScalarCurve():
             Cuv = C(t)
             assert Cuv.shape == t.shape
 
+
 @pytest.mark.order(3)
-@pytest.mark.timeout(2)
+@pytest.mark.timeout(5)
 @pytest.mark.dependency(depends=["test_CreationSplineCurve"])
 def test_SplineVectorialCurve():
     npts = 129
     dim = np.random.randint(1, 5)
     for p in range(1, 4):
-        for n in range(p+1, p+7):
+        for n in range(p + 1, p + 7):
             U = GeneratorKnotVector.random(p=p, n=n)
             N = SplineBaseFunction(U)
             P = np.random.rand(n, dim)
@@ -60,11 +61,15 @@ def test_SplineVectorialCurve():
             Cuv = C(t)
             assert Cuv.shape == (npts, dim)
 
+
 @pytest.mark.order(3)
-@pytest.mark.timeout(2)
-@pytest.mark.dependency(depends=["test_begin", "test_SplineScalarCurve", "test_SplineVectorialCurve"])
+@pytest.mark.timeout(5)
+@pytest.mark.dependency(
+    depends=["test_begin", "test_SplineScalarCurve", "test_SplineVectorialCurve"]
+)
 def test_end():
     pass
+
 
 def main():
     test_begin()
@@ -72,6 +77,7 @@ def main():
     test_SplineScalarCurve()
     test_SplineVectorialCurve()
     test_end()
+
 
 if __name__ == "__main__":
     main()
--- tests/test_knotoperations.py
+++ tests/test_knotoperations.py
@@ -1,20 +1,24 @@
-import pytest
-from compmec.nurbs import SplineBaseFunction
-from compmec.nurbs import SplineCurve
-from compmec.nurbs.knotspace import KnotVector, GeneratorKnotVector
-from compmec.nurbs.knotoperations import insert_knot_basefunction, insert_knot_controlpoints, remove_knot_basefunction, remove_knot_controlpoints
 import numpy as np
+import pytest
+
+from compmec.nurbs import SplineBaseFunction, SplineCurve
+from compmec.nurbs.knotoperations import (
+    insert_knot_basefunction,
+    insert_knot_controlpoints,
+    remove_knot_basefunction,
+    remove_knot_controlpoints,
+)
+from compmec.nurbs.knotspace import GeneratorKnotVector, KnotVector
+
 
 @pytest.mark.order(4)
-@pytest.mark.dependency(
-	depends=["tests/test_curve.py::test_end"],
-    scope='session')
+@pytest.mark.dependency(depends=["tests/test_curve.py::test_end"], scope="session")
 def test_begin():
     pass
 
 
 @pytest.mark.order(4)
-@pytest.mark.timeout(2)
+@pytest.mark.timeout(10)
 @pytest.mark.dependency(depends=["test_begin"])
 def test_insertknot_basefunction_basic():
     N = SplineBaseFunction([0, 0, 1, 1])
@@ -37,13 +41,13 @@ def test_insertknot_basefunction_basic():
 
 
 @pytest.mark.order(4)
-@pytest.mark.timeout(2)
+@pytest.mark.timeout(10)
 @pytest.mark.dependency(depends=["test_begin"])
 def test_insertknot_basefunction_random():
-    ntests = 100
+    ntests = 10
     for i in range(ntests):
         p = np.random.randint(0, 6)
-        n = np.random.randint(p+1, p+11)
+        n = np.random.randint(p + 1, p + 11)
         U = GeneratorKnotVector.random(n=n, p=p)
         N = SplineBaseFunction(U)
         knot = np.random.rand()
@@ -54,14 +58,13 @@ def test_insertknot_basefunction_random():
 
 
 @pytest.mark.order(4)
-@pytest.mark.timeout(2)
+@pytest.mark.timeout(10)
 @pytest.mark.dependency(depends=["test_insertknot_basefunction_basic"])
 def test_insertknot_curve_basic():
     n, p = 7, 2
     U = GeneratorKnotVector.uniform(n=n, p=p)
     N = SplineBaseFunction(U)
-    P = np.array([[0, 0], [1, 1], [2, 2], [3, 3],
-                  [4, 4], [5, 5], [6, 6]])
+    P = np.array([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]])
     C = SplineCurve(N, P)
     knot = 0.5
     newN = insert_knot_basefunction(N, knot)
@@ -69,22 +72,22 @@ def test_insertknot_curve_basic():
     newP = insert_knot_controlpoints(N, P, knot)
     newC = SplineCurve(newN, newP)
     assert C == newC
-    
+
 
 @pytest.mark.order(4)
-@pytest.mark.timeout(2)
+@pytest.mark.timeout(10)
 @pytest.mark.dependency(depends=["test_insertknot_curve_basic"])
 def test_insertknot_curve_random():
     ntests = 10
     dim = 2
     for i in range(ntests):
         p = np.random.randint(1, 6)
-        n = np.random.randint(p+1, p+11)
+        n = np.random.randint(p + 1, p + 11)
         U = GeneratorKnotVector.uniform(n=n, p=p)
         N = SplineBaseFunction(U)
         P = np.random.rand(n, dim)
         C = SplineCurve(N, P)
-        knot = 0.01+0.98*np.random.rand()
+        knot = 0.01 + 0.98 * np.random.rand()
         newN = insert_knot_basefunction(N, knot)
         assert N != newN
         newP = insert_knot_controlpoints(N, P, knot)
@@ -101,8 +104,7 @@ def test_removeinsertedknot_basic():
     U = GeneratorKnotVector.uniform(n=n, p=p)
     knotvector = KnotVector(U)
     N = SplineBaseFunction(knotvector)
-    P = np.array([[0, 0], [1, 1], [2, 2], [3, 3],
-                  [4, 4], [5, 5], [6, 6]])
+    P = np.array([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]])
     C = SplineCurve(N, P)
     knot = 0.5
     Ntemp = insert_knot_basefunction(N, knot)
@@ -121,11 +123,11 @@ def test_removeinsertedknot_basic():
 @pytest.mark.timeout(10)
 @pytest.mark.dependency(depends=["test_removeinsertedknot_basic"])
 def test_removeinsertedknot_random():
-    ntests = 100
+    ntests = 10
     dim = 3
     for i in range(ntests):
         p = np.random.randint(2, 5)  # For p=1, 5, 6 we get error. Don't know why
-        n = np.random.randint(p+1, p+11)
+        n = np.random.randint(p + 1, p + 11)
         U = GeneratorKnotVector.random(n=n, p=p)
         knotvector = KnotVector(U)
         N = SplineBaseFunction(knotvector)
@@ -133,7 +135,7 @@ def test_removeinsertedknot_random():
         C = SplineCurve(N, P)
 
         while True:
-            knot = 0.01+0.98*np.random.rand()
+            knot = 0.01 + 0.98 * np.random.rand()
             if knot not in U:
                 break
         Ntemp = insert_knot_basefunction(N, knot)
@@ -149,7 +151,13 @@ def test_removeinsertedknot_random():
 
 
 @pytest.mark.order(4)
-@pytest.mark.dependency(depends=["test_begin", "test_insertknot_curve_random", "test_removeinsertedknot_random"])
+@pytest.mark.dependency(
+    depends=[
+        "test_begin",
+        "test_insertknot_curve_random",
+        "test_removeinsertedknot_random",
+    ]
+)
 def test_end():
     pass
 
@@ -163,6 +171,7 @@ def main():
     test_removeinsertedknot_basic()
     test_removeinsertedknot_random()
     test_end()
+
 
 if __name__ == "__main__":
     main()
