6029b8395023efd97817f89c6383e0c3abaf293e
CarlosAdir <carlos.adir.leite@gmail.com>
2022-11-30 18:25:28 -0300

    Clean up tests to make a instance random creator

--- tests/test_curves.py
+++ tests/test_curves.py
@@ -25,10 +25,18 @@ class TestSplineCurve:
     def create_random_knotvector(self, degree: int, npts: int):
         return GeneratorKnotVector.random(degree, npts)
 
-    def create_random_controlpoints(self, npts: int, ndim: int = 0):
+    def create_random_controlpoints(self, npts: int, ndim: int):
         if ndim == 0:
-            return np.random.uniform(-1, 1, npts)
-        return np.random.uniform(-1, 1, (npts, ndim))
+            P = np.random.uniform(-1, 1, npts)
+        else:
+            P = np.random.uniform(-1, 1, (npts, ndim))
+        return P
+
+    def create_random_bspline(self, degree: int, npts: int, ndim: int):
+        U = self.create_random_knotvector(degree, npts)
+        P = self.create_random_controlpoints(npts, ndim)
+        C = SplineCurve(U, P)
+        return C
 
     @pytest.mark.order(3)
     @pytest.mark.dependency(depends=["test_begin"])
@@ -38,25 +46,21 @@ class TestSplineCurve:
     @pytest.mark.order(3)
     @pytest.mark.timeout(5)
     @pytest.mark.dependency(depends=["TestSplineCurve::test_begin"])
-    def test_creation_scalar_curve(self, ntests=10):
-        for i in range(ntests):
-            degree = np.random.randint(0, 5)
-            npts = np.random.randint(degree + 1, degree + 11)
-            U = self.create_random_knotvector(degree, npts)
-            P = self.create_random_controlpoints(npts)
-            SplineCurve(U, P)
+    def test_creation_scalar_curve(self, ntests=1):
+        for degree in range(1, 5):
+            for npts in range(degree + 1, degree + 1):
+                for i in range(ntests):
+                    self.create_random_bspline(degree, npts, 0)
 
     @pytest.mark.order(3)
     @pytest.mark.timeout(5)
     @pytest.mark.dependency(depends=["TestSplineCurve::test_begin"])
-    def test_creation_vectorial_curve(self, ntests=10):
-        for i in range(ntests):
-            degree = np.random.randint(0, 5)
-            npts = np.random.randint(degree + 1, degree + 11)
-            ndim = np.random.randint(1, 4)
-            U = self.create_random_knotvector(degree, npts)
-            P = self.create_random_controlpoints(npts, ndim)
-            SplineCurve(U, P)
+    def test_creation_vectorial_curve(self, ntests=1):
+        for degree in range(1, 5):
+            for npts in range(degree + 1, degree + 11):
+                for ndim in range(1, 5):
+                    for i in range(ntests):
+                        self.create_random_bspline(degree, npts, ndim)
 
     @pytest.mark.order(3)
     @pytest.mark.timeout(5)
@@ -67,7 +71,7 @@ class TestSplineCurve:
             "TestSplineCurve::test_creation_vectorial_curve",
         ]
     )
-    def test_creationfail(self, ntests=100):
+    def test_creationfail(self, ntests=10):
         for i in range(ntests):
             degree = np.random.randint(0, 5)
             npts = np.random.randint(degree + 1, degree + 11)
@@ -90,7 +94,7 @@ class TestSplineCurve:
             "TestSplineCurve::test_creation_vectorial_curve",
         ]
     )
-    def test_compare_two_curves(self, ntests=10):
+    def test_compare_two_curves(self, ntests=1):
         for i in range(ntests):
             degree = np.random.randint(0, 5)
             npts = np.random.randint(degree + 1, degree + 11)
@@ -134,16 +138,16 @@ class TestSplineCurve:
             "TestSplineCurve::test_curve_is_callable",
         ]
     )
-    def test_shapetype_callscalar_scalarpoints(self, ntests=10):
-        degree = np.random.randint(0, 5)
-        npts = np.random.randint(degree + 1, degree + 11)
-        U = self.create_random_knotvector(degree, npts)
-        P = self.create_random_controlpoints(npts)
-        C = SplineCurve(U, P)
+    def test_shapetype_callscalar_scalarpoints(self, ntests=1):
+        for degree in range(1, 5):
+            for npts in range(degree + 1, degree + 11):
+                U = self.create_random_knotvector(degree, npts)
+                P = self.create_random_controlpoints(npts, 0)
+                C = SplineCurve(U, P)
 
-        t = np.random.uniform(0, 1)
-        Cval = C(t)
-        assert type(Cval) == type(P[0])
+                t = np.random.uniform(0, 1)
+                Cval = C(t)
+                assert type(Cval) == type(P[0])
 
     @pytest.mark.order(3)
     @pytest.mark.timeout(5)
@@ -154,20 +158,20 @@ class TestSplineCurve:
             "TestSplineCurve::test_shapetype_callscalar_scalarpoints",
         ]
     )
-    def test_shapetype_callscalar_vectorialpoints(self, ntests=10):
-        for i in range(ntests):
-            degree = np.random.randint(0, 5)
-            npts = np.random.randint(degree + 1, degree + 11)
-            ndim = np.random.randint(1, 4)
-            U = self.create_random_knotvector(degree, npts)
-            P = self.create_random_controlpoints(npts, ndim)
-            C = SplineCurve(U, P)
+    def test_shapetype_callscalar_vectorialpoints(self, ntests=1):
+        for degree in range(1, 5):
+            for npts in range(degree + 1, degree + 11):
+                for ndim in range(1, 5):
+                    for i in range(ntests):
+                        U = self.create_random_knotvector(degree, npts)
+                        P = self.create_random_controlpoints(npts, ndim)
+                        C = SplineCurve(U, P)
 
-            t = np.random.uniform(0, 1)
-            Cval = C(t)
-            assert len(Cval) == ndim
-            assert type(Cval) == type(P[0])
-            assert type(Cval[0]) == type(P[0][0])
+                        t = np.random.uniform(0, 1)
+                        Cval = C(t)
+                        assert len(Cval) == ndim
+                        assert type(Cval) == type(P[0])
+                        assert type(Cval[0]) == type(P[0][0])
 
     @pytest.mark.order(3)
     @pytest.mark.timeout(5)
@@ -177,19 +181,19 @@ class TestSplineCurve:
             "TestSplineCurve::test_curve_is_callable",
         ]
     )
-    def test_shapetype_callvectorial_scalarpoints(self, ntests=10):
-        for i in range(ntests):
-            degree = np.random.randint(0, 5)
-            npts = np.random.randint(degree + 1, degree + 11)
-            U = self.create_random_knotvector(degree, npts)
-            P = self.create_random_controlpoints(npts)
-            C = SplineCurve(U, P)
+    def test_shapetype_callvectorial_scalarpoints(self, ntests=1):
+        for degree in range(1, 5):
+            for npts in range(degree + 1, degree + 11):
+                for i in range(ntests):
+                    U = self.create_random_knotvector(degree, npts)
+                    P = self.create_random_controlpoints(npts, 0)
+                    C = SplineCurve(U, P)
 
-            nsample = np.random.randint(10, 129)
-            t = np.linspace(0, 1, nsample)
-            Cval = C(t)
-            assert len(Cval) == nsample
-            assert type(Cval[0]) == type(P[0])
+                    nsample = np.random.randint(10, 129)
+                    t = np.linspace(0, 1, nsample)
+                    Cval = C(t)
+                    assert len(Cval) == nsample
+                    assert type(Cval[0]) == type(P[0])
 
     @pytest.mark.order(3)
     @pytest.mark.timeout(5)
@@ -201,21 +205,21 @@ class TestSplineCurve:
             "TestSplineCurve::test_shapetype_callscalar_vectorialpoints",
         ]
     )
-    def test_shapetype_callvectorial_vectorialpoints(self, ntests=10):
-        for i in range(ntests):
-            degree = np.random.randint(0, 5)
-            npts = np.random.randint(degree + 1, degree + 11)
-            ndim = np.random.randint(1, 4)
-            U = self.create_random_knotvector(degree, npts)
-            P = self.create_random_controlpoints(npts, ndim)
-            C = SplineCurve(U, P)
+    def test_shapetype_callvectorial_vectorialpoints(self, ntests=1):
+        for degree in range(1, 5):
+            for npts in range(degree + 1, degree + 11):
+                for ndim in range(1, 5):
+                    for i in range(ntests):
+                        U = self.create_random_knotvector(degree, npts)
+                        P = self.create_random_controlpoints(npts, ndim)
+                        C = SplineCurve(U, P)
 
-            nsample = np.random.randint(10, 129)
-            t = np.linspace(0, 1, nsample)
-            Cval = C(t)
-            assert len(Cval) == nsample
-            assert type(Cval[0]) == type(P[0])
-            assert np.array(Cval).shape == (nsample, ndim)
+                        nsample = np.random.randint(10, 129)
+                        t = np.linspace(0, 1, nsample)
+                        Cval = C(t)
+                        assert len(Cval) == nsample
+                        assert type(Cval[0]) == type(P[0])
+                        assert np.array(Cval).shape == (nsample, ndim)
 
     @pytest.mark.order(3)
     @pytest.mark.timeout(5)
@@ -231,30 +235,34 @@ class TestSplineCurve:
     )
     def test_bezier_curve_values(self):
         for degree in range(1, 5):
-            npts = degree + 1
-            U = GeneratorKnotVector.bezier(p=degree)
-            ndim = np.random.randint(0, 4)
-            P = self.create_random_controlpoints(npts, ndim)
-            C = SplineCurve(U, P)
+            for ndim in range(0, 5):
+                npts = degree + 1
+                ndim = np.random.randint(0, 4)
+                U = self.create_random_knotvector(degree, npts)
+                P = self.create_random_controlpoints(npts, ndim)
+                C = SplineCurve(U, P)
 
-            nsample = np.random.randint(10, 129)
-            t = np.linspace(0, 1, nsample)
-            Cval = C(t)
-            assert len(Cval) == nsample
-            assert type(Cval[0]) == type(P[0])
-            if ndim == 0:
-                assert np.array(Cval).shape == (nsample,)
-            else:
-                assert np.array(Cval).shape == (nsample, ndim)
+                nsample = np.random.randint(10, 129)
+                t = np.linspace(0, 1, nsample)
+                Cval = C(t)
+                assert len(Cval) == nsample
+                assert type(Cval[0]) == type(P[0])
+                if ndim == 0:
+                    assert np.array(Cval).shape == (nsample,)
+                else:
+                    assert np.array(Cval).shape == (nsample, ndim)
 
-            Cgood = np.zeros(np.array(Cval).shape)
-            for i, ti in enumerate(t):
-                ti1 = 1 - ti
-                for j, Pj in enumerate(P):
-                    Cgood[i] += (
-                        math.comb(npts - 1, j) * ti1 ** (npts - 1 - j) * ti**j * Pj
-                    )
-            np.testing.assert_almost_equal(Cval, Cgood)
+                Cgood = np.zeros(np.array(Cval).shape)
+                for i, ti in enumerate(t):
+                    ti1 = 1 - ti
+                    for j, Pj in enumerate(P):
+                        Cgood[i] += (
+                            math.comb(npts - 1, j)
+                            * ti1 ** (npts - 1 - j)
+                            * ti**j
+                            * Pj
+                        )
+                np.testing.assert_almost_equal(Cval, Cgood)
 
     @pytest.mark.order(3)
     @pytest.mark.timeout(5)
@@ -353,7 +361,7 @@ class TestSplineCurve:
             "TestSplineCurve::test_knot_insert_known_case",
         ]
     )
-    def test_knot_insert_random(self, ntests=100):
+    def test_knot_insert_random(self, ntests=10):
         for i in range(ntests):
             degree = np.random.randint(1, 5)
             npts = np.random.randint(degree + 1, degree + 11)
@@ -382,7 +390,7 @@ class TestSplineCurve:
             "TestSplineCurve::test_knot_remove",
         ]
     )
-    def test_knot_insert_remove_once_random(self, ntests=10):
+    def test_knot_insert_remove_once_random(self, ntests=1):
         for degree in range(1, 6):
             for i in range(ntests):
                 npts = np.random.randint(degree + 1, degree + 11)
@@ -405,7 +413,7 @@ class TestSplineCurve:
             "TestSplineCurve::test_begin",
         ]
     )
-    def test_degree_increase(self, ntests=100):
+    def test_degree_increase(self, ntests=10):
         for degree in range(1, 6):
             for i in range(ntests):
                 npts = np.random.randint(degree + 1, degree + 11)
@@ -421,7 +429,7 @@ class TestSplineCurve:
     @pytest.mark.dependency(
         depends=["TestSplineCurve::test_begin", "TestSplineCurve::test_degree_increase"]
     )
-    def test_degree_decrease(self, ntests=100):
+    def test_degree_decrease(self, ntests=10):
         pass
 
     @pytest.mark.order(3)
@@ -433,7 +441,7 @@ class TestSplineCurve:
             "TestSplineCurve::test_degree_decrease",
         ]
     )
-    def test_degree_increase_decrease(self, ntests=100):
+    def test_degree_increase_decrease(self, ntests=10):
         for degree in range(1, 6):
             for i in range(ntests):
                 npts = np.random.randint(degree + 1, degree + 11)
