fa847da589dc55df8dade99cdf8fac1142e821f0
carlos-adir <carlos.adir.leite@gmail.com>
2023-07-07 22:25:23 +0200

    Update version to v1.0.5

--- src/compmec/nurbs/__init__.py
+++ src/compmec/nurbs/__init__.py
@@ -2,7 +2,7 @@ from compmec.nurbs.curves import Curve
 from compmec.nurbs.functions import Function
 from compmec.nurbs.knotspace import GeneratorKnotVector, KnotVector
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 if __name__ == "__main__":
     pass
