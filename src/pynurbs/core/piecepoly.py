927a062e72277ba24219ad5846b0c51fa83596cc
Carlos Adir <carlos.adir.leite@gmail.com>
2025-06-30 22:36:22 +0200

    feat: implement __add__, __sub__, __mul__ on piecewise

--- src/pynurbs/core/piecepoly.py
+++ src/pynurbs/core/piecepoly.py
@@ -2,10 +2,47 @@
 Defines the Piecewise Polynomial class
 """
 
+from __future__ import annotations
+
 from numbers import Real
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, Union
 
 from .polynomial import Polynomial
+from .tools import vectorize
+
+
+def find_span(node: Real, knots: Tuple[Real, ...]):
+    """
+    Finds the span of the given node
+
+    Example
+    -------
+    >>> knots = [0, 1, 3, 4]
+    >>> find_span(-1, knots)
+    -1
+    >>> find_span(0, knots)
+    0
+    >>> find_span(0.5, knots)
+    0
+    >>> find_span(1, knots)
+    1
+    >>> find_span(2, knots)
+    1
+    >>> find_span(3, knots)
+    2
+    >>> find_span(4, knots)
+    2
+    >>> find_span(5, knots)
+    3
+    """
+    if node < knots[0]:
+        return -1
+    if knots[-1] < node:
+        return len(knots) - 1
+    for i, knot in enumerate(knots[:-1]):
+        if knot <= node:
+            return i
+    return len(knots) - 2
 
 
 class PiecewisePolynomial:
@@ -25,13 +62,70 @@ class PiecewisePolynomial:
     def functions(self) -> Tuple[Polynomial, ...]:
         return self.__functions
 
-    def eval(self, node: Real, times: int = 0) -> Real:
-        nsegs = len(self.functions)
-        mask = self.knots[nsegs - 1] <= node
-        mask *= node < self.knots[nsegs]
-        result = mask * self.functions[-1](node, times)
-        for i in range(nsegs - 1):
-            mask = self.knots[i] <= node
-            mask *= node < self.knots[i + 1]
-            result += mask * self.functions[i](node, times)
-        return result
+    @vectorize(1, 0)
+    def __call__(self, node: Real) -> Real:
+        span = find_span(node, self.knots)
+        function = self.functions[span]
+        return function(node)
+
+    def __neg__(self) -> Polynomial:
+        return self.__class__((-func for func in self.functions), self.knots)
+
+    def __add__(
+        self, other: Union[Real, Polynomial, PiecewisePolynomial]
+    ) -> PiecewisePolynomial:
+        if not isinstance(other, PiecewisePolynomial):
+            return self.__class__((func + other for func in self.functions), self.knots)
+        allknots = sorted(set(self.knots) | set(other.knots))
+        functions = [None] * (len(allknots) - 1)
+        for i, (knota, knotb) in enumerate(zip(allknots, allknots[1:])):
+            midknot = (knota + knotb) / 2
+            spana = find_span(midknot, self.knots)
+            spanb = find_span(midknot, self.knots)
+            functions[i] = self.functions[spana] + other.functions[spanb]
+        return self.__class__(functions, allknots)
+
+    def __mul__(
+        self, other: Union[Real, Polynomial, PiecewisePolynomial]
+    ) -> PiecewisePolynomial:
+        if not isinstance(other, PiecewisePolynomial):
+            return self.__class__((func + other for func in self.functions), self.knots)
+        allknots = sorted(set(self.knots) | set(other.knots))
+        functions = [None] * (len(allknots) - 1)
+        for i, (knota, knotb) in enumerate(zip(allknots, allknots[1:])):
+            midknot = (knota + knotb) / 2
+            spana = find_span(midknot, self.knots)
+            spanb = find_span(midknot, self.knots)
+            functions[i] = self.functions[spana] * other.functions[spanb]
+        return self.__class__(functions, allknots)
+
+    def __matmul__(
+        self, other: Union[Real, Polynomial, PiecewisePolynomial]
+    ) -> PiecewisePolynomial:
+        if not isinstance(other, PiecewisePolynomial):
+            return self.__class__((func + other for func in self.functions), self.knots)
+        allknots = sorted(set(self.knots) | set(other.knots))
+        functions = [None] * (len(allknots) - 1)
+        for i, (knota, knotb) in enumerate(zip(allknots, allknots[1:])):
+            midknot = (knota + knotb) / 2
+            spana = find_span(midknot, self.knots)
+            spanb = find_span(midknot, self.knots)
+            functions[i] = self.functions[spana] @ other.functions[spanb]
+        return self.__class__(functions, allknots)
+
+    def __sub__(
+        self, other: Union[Real, Polynomial, PiecewisePolynomial]
+    ) -> PiecewisePolynomial:
+        return self.__add__(-other)
+
+    def __rsub__(self, other: Real) -> PiecewisePolynomial:
+        return (-self).__add__(other)
+
+    def __radd__(self, other: Real) -> PiecewisePolynomial:
+        return self.__add__(other)
+
+    def __rmul__(self, other: Real) -> PiecewisePolynomial:
+        return self.__mul__(other)
+
+    def __rmatmul__(self, other: Real) -> PiecewisePolynomial:
+        return self.__matmul__(other)
