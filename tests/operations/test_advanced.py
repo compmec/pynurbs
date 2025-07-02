330273db718e10377c0843f3436d55baf85cf9ae
Carlos Adir <carlos.adir.leite@gmail.com>
2025-07-02 22:18:24 +0200

    test: fix test dependency for advanced

--- tests/operations/test_advanced.py
+++ tests/operations/test_advanced.py
@@ -13,11 +13,11 @@ from pynurbs.responsive.curves import Curve
 @pytest.mark.order(42)
 @pytest.mark.dependency(
     depends=[
-        "tests/test_knotspace.py::test_end",
-        "tests/test_functions.py::test_end",
-        "tests/test_beziercurve.py::test_end",
-        "tests/test_splinecurve.py::test_end",
-        "tests/test_calculus.py::test_end",
+        "tests/responsive/test_knotspace.py::test_end",
+        "tests/responsive/test_functions.py::test_end",
+        "tests/responsive/test_beziercurve.py::test_end",
+        "tests/responsive/test_splinecurve.py::test_end",
+        "tests/operations/test_calculus.py::test_end",
     ],
     scope="session",
 )
